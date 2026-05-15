449bf251511c28b1e4eb2f3abc7548a542aac3bb
Junman <86673505+Junman140@users.noreply.github.com>
2026-05-15 17:40:41 +0100

    asdfghjk

--- cmd/stellar-rpc/lib/preflight/src/lib.rs
+++ cmd/stellar-rpc/lib/preflight/src/lib.rs
@@ -38,7 +38,7 @@ mod curr {
     #[allow(clippy::duplicate_mod)]
     pub(crate) mod shared;
 
-    pub(crate) const PROTOCOL: u32 = soroban_env_host::meta::INTERFACE_VERSION as u32;
+    pub(crate) const PROTOCOL: u32 = 23;
 
     pub(crate) fn load_network_config(
         snapshot: &(impl soroban_env_host::storage::SnapshotSource + soroban_simulation::SnapshotSourceWithArchive),
